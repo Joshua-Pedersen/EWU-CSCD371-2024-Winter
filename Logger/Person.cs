8cac51ef9ca75c8126fd6d9079757d00006b9b7a
Josh <jpedersen@ewu.edu>
2024-02-05 13:08:27 -0800

    Added commment

--- Logger/Person.cs
+++ Logger/Person.cs
@@ -2,6 +2,7 @@
 
 public record class Person(FullName fullName) : BaseEntity
 {
+    // Implicit as we are passing in the fullname to be used.
     public override string Name { get => fullName.Name; }
     
 }
