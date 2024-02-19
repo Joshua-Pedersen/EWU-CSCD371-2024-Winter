e0f327f2c13223ff1f91a1db6c0b46ad0c9c5092
Josh <jpedersen@ewu.edu>
2024-02-19 14:37:58 -0800

    Read/Write
    Added WriteLine, ReadLine, and respective tests

--- Calculate/Calculate.Tests/ProgramTests.cs
+++ Calculate/Calculate.Tests/ProgramTests.cs
@@ -4,5 +4,37 @@ namespace Calculate.Tests;
 
 public class ProgramTests
 {
+    [Fact]
+    public void Program_WriteLine_OutputIsEqual()
+    {
+        // Arrange
+        StringWriter capture = new();
+        Console.SetOut(capture);
 
+        Program testProg = new();
+
+        string testMessage = "foo";
+
+        // Act
+        testProg.WriteLine(testMessage);
+
+        // Assert
+        Assert.Equal(testMessage, capture.ToString().Trim());
+    }
+
+    [Fact]
+    public void Program_ReadLine_InputIsEqual()
+    {
+        // Arrange
+        string testMessage = "foo";
+        StringReader capture = new(testMessage);
+        Console.SetIn(capture);
+        Program testProg = new();
+
+        // Act
+        string? readString = testProg.ReadLine();
+
+        // Assert
+        Assert.Equal(testMessage, readString);
+    }
 }
--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -3,6 +3,11 @@ namespace Calculate;
 
 public class Program
 {
+    public Program() { }
+
+    public Action<string> WriteLine { get; init; } = Console.WriteLine;
+    public Func<string?> ReadLine { get; init; } = Console.ReadLine;
+
     public static void Main(string[] args)
     {
 
