a273b20ce70443423945d2936999ba833129ef54
Josh <jpedersen@ewu.edu>
2024-03-05 23:52:50 -0800

    Almost

---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment;
+public class Node<T> : IEnumerable<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        string contents = $"{Value}";
+        return contents;
+    }
+
+    public void Append(T value)
+    {
+        if (Exists(value))
+        {
+            throw new System.ArgumentException("Duplicate Value");
+        }
+
+
+        Node<T> newNode = new(value);
+        Node<T> currentNode = this;
+
+        while (currentNode.Next != this)
+        {
+            currentNode = currentNode.Next;
+        }
+
+        currentNode.Next = newNode;
+        newNode.Next = this;
+    }
+
+    public void Clear()
+    {
+        if (this.Next == this)
+        {
+            return;
+        }
+
+        Node<T> currentNode = this;
+
+        while (currentNode.Next != this)
+        {
+            currentNode = currentNode.Next;
+        }
+
+        currentNode.Next = this.Next;
+        this.Next = this;
+    }
+
+    public bool Exists(T value)
+    {
+        Node<T> currentNode = this;
+
+        if (currentNode.Value!.Equals(value))
+        {
+            return true;
+        }
+
+        currentNode = currentNode.Next;
+
+        while (currentNode != this)
+        {
+            if (currentNode.Value!.Equals(value))
+            {
+                return true;
+            }
+            currentNode = currentNode.Next;
+        }
+
+        return false;
+
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw GetEnumerator();
+    }
+
+    public IEnumerable<T> ChildItems(int maximumL)
+    {
+
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace Assignment
+namespace Assignment;
+
+public class SampleData : ISampleData
 {
-    public class SampleData : ISampleData
-    {
-        // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+    // 1.
+    public IEnumerable<string> CsvRows => File.ReadAllLines("People.csv").Skip(1);
 
-        // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+    // 2.
+    public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        => CsvRows.Select(item => item.Split(',')[6]).Distinct().Order();
 
-        // 3.
-        public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+    // 3.
+    public string GetAggregateSortedListOfStatesUsingCsvRows()
+        => String.Join(',', GetUniqueSortedListOfStatesGivenCsvRows().ToArray());
 
-        // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+    // 4.
+    public IEnumerable<IPerson> People =>
+        (from id in CsvRows
+         let item = id.Split(',')
+         orderby item[6], item[5], item[7]
+         select new Person(item[1], item[2], new Address(item[4], item[5], item[6], item[7]), item[3])
+         );
 
-        // 5.
-        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
+     // 5.
+     public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
+         Predicate<string> filter) => People.Where(person =>filter(person.EmailAddress)).Select(person =>(person.FirstName, person.LastName));
 
-        // 6.
-        public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
-    }
+    // 6.
+    public string GetAggregateListOfStatesGivenPeopleCollection(
+        IEnumerable<IPerson> people) => people.Select(person => person.Address.State).Distinct().Aggregate((list, state) => $"{list}, {state}");
 }
+
